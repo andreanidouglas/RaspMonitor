6beab0f102d680ba4d3f493fa986ece58c758b32
Douglas R Andreani <cmtedouglas@hotmail.com>
2014-03-07 23:38:07 -0300

    resetPins

--- temp_monitor.c
+++ temp_monitor.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #define MAX_TEMP 45000
-#define PIN_TEMPERATURA 14
+#define PIN_TEMPERATURA 7
 long readNumberFile (char *filePath);
 void TemperaturaMonitor();
+void resetPins();
 int main ()
 {
 	wiringPiSetup(); //setup IO
+	resetPins();
 	int pid;
 	pid = fork();
 	if (pid < 0)
@@ -58,5 +60,17 @@ void TemperaturaMonitor()
 		{
 			digitalWrite(PIN_TEMPERATURA, HIGH);
 		}
+	}
+}
+
+void resetPins()
+{
+	int i;
+	for (i=0;i<=7;i++)
+	{
+		pinMode(i, OUTPUT);
+		digitalWrite(i, HIGH);
+		delay(50);
+		digitalWrite(i, LOW);
 	}
 }
